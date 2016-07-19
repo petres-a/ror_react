d8d3c58e32ca7ecfebbd82d2f3796b5f9f2ef8fe
Andrei Petrescu <petres_a@epitech.eu>
2016-07-19 13:18:54 +0200

    pretty much all i need for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,9 @@
 class CommentsController < ApplicationController
 
+  def index
+    @comments = Comment.where(restaurant_id: params[:restaurant_id])
+  end
+
   # remember this for your project
   def create
     @comment = Comment.create(comment_params.merge(restaurant_id: params[:restaurant_id]))
